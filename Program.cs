451576e90c53a075600131a109092185bd7a9085
JongD <JongD@JONGDO>
2024-06-06 22:28:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment5._2._2
+{
+    internal class Program
+    {
+                                                                                // Main method, the entry point of the program
+        static void Main()
+        {                                                                     // Display a description of the program   
+            Console.Write("How many numbers to print: ");
+            int numtoPrint = Convert.ToInt32(Console.ReadLine());                                   // Read the user input for the count of numbers
+                                                                               // Call the recursive method to print the first 'ctr' natural numbers starting from 1
+            printNatural(1, numtoPrint);
+            
+        }
+        //start at 1 ten numbers to print
+                                                                             
+        static int printNatural(int startingValue, int numtoPrint)
+        {                                                                                     
+            if (numtoPrint < 1)
+            {
+                return startingValue;
+            }                                                                  
+            Console.Write($" {startingValue} ");                              
+            numtoPrint--;                                                          
+            return printNatural(startingValue + 1, numtoPrint);
+        }
+
+        
+    }
+}
